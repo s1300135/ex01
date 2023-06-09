2c7ab2addb9577126ad3c1bd94b87987da675083
Goto sho <s1300135@u-aizu.ac.jp>
2023-06-09 16:15:29 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,15 @@
+import java.utiol.Random;
+class DiceGame{
+    public statc void main(String[] args){
+	Random rand = new Random();
+
+	int n1 = rand.nextInt(6) + 1;
+	int n2 = rand.nextInt(6) + 1;
+	int total = n1 + n2;
+
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: " + n1);
+	System.out.println("Die 2: " + n2);
+	System.out.println("Total value: " + total);
+    }
+}
